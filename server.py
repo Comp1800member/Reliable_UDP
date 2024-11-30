ec2586bc072ac080a6a3f7d9efcebe52223b67db
jonathanliu <Jonathanliu865@gmail.com>
2024-11-30 11:20:43 -0800

    Implemented basic select poll in server.py

--- server.py
+++ server.py
@@ -1,33 +1,97 @@
-import socket
+import socket, select, os, argparse, threading, ipaddress
 from utils import compile_packet, get_fields, INIT_PACKET
 
-# Define the IP address and port for the server
-server_ip = "127.0.0.1"  # Localhost
-server_port = 12345
+def handle_arguments(args):
+    ip = args.listen_ip
+    if args.listen_port < 1 or args.listen_port > 65535:
+        print('port must be between 1 and 65535')
+        exit(-1)
+    try:
+        ipaddress.ip_address(ip)
+    except ValueError:
+        print("Invalid IP address")
 
-# Create a UDP socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Bind the socket to the IP address and port
-server_socket.bind((server_ip, server_port))
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--listen-ip", type=str, help="IP address of the server")
+    parser.add_argument("--listen-port", type=int, help="The port number")
+    args = parser.parse_args()
+    print(args)
+    handle_arguments(args)
+    return args
 
-print(f"UDP server is up and listening on {server_ip}:{server_port}")
+
+def create_socket():
+    # note to self, changed to DGRAM for UDP
+    return socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+def bind_socket(server_socket, ip_addr, port):
+    try:
+        server_socket.bind((ip_addr, port))
+    except OSError as e:
+        print(f"Binding failed: {e}")
+        exit(-1)
+
+
+def close_socket(sock_to_close):
+    try:
+        sock_to_close.close()
+    except OSError as e:
+        print(e)
+        exit(-1)
+
+
+def receive_data(server_socket):
+    try:
+        data, client_address = server_socket.recvfrom(1024)
+    except Exception as e:
+        print(e)
+        exit(-1)
+    return data, client_address
+
+
+def send_ack():
+    pass
+
 
 set_delay = False
 seq_num = INIT_PACKET
 
-while True:
-    # Wait for a message from a client
-    data, client_address = server_socket.recvfrom(1024)  # Buffer size of 1024 bytes
-    print(f"Received message: {data.decode()} from {client_address}")
+if __name__ == '__main__':
 
-    # Test: delay scenario
-    # if not set_delay:
-    #     time.sleep(5)
-    #     set_delay = True
 
-    _, _, seq_num, payload = get_fields(data)
-    print(f"Payload from Client: {payload}")
-    packet_to_send = compile_packet(seq_num, len(payload), "")
-    server_socket.sendto(packet_to_send, client_address)
-    print(f"Sent message \"{packet_to_send}\" to {client_address}")
+    args = parse_arguments()
+    port = args.listen_port
+    ip_addr = args.listen_ip
+    server_socket = create_socket()
+
+    bind_socket(server_socket, ip_addr, port)
+
+    try:
+        while True:
+            ready, _, _ = select.select([socket], [], [])
+            for sock in ready:
+                data, client_addr = server_socket.recvfrom(1024)  # Buffer size of 1024 bytes
+                print(f"Received '{data.decode()} from {client_addr}")
+
+                response = f"Hello, {data.decode()}"
+                server_socket.sendto(response.encode(), client_addr)
+
+                # Test: delay scenario
+                # if not set_delay:
+                #     time.sleep(5)
+                #     set_delay = True
+
+                _, _, seq_num, payload = get_fields(data)
+                print(f"Payload from Client: {payload}")
+                packet_to_send = compile_packet(seq_num, len(payload), "")
+                server_socket.sendto(packet_to_send, client_addr)
+                print(f"Sent message \"{packet_to_send}\" to {client_addr}")
+
+    except KeyboardInterrupt:
+        print("\nKeyboard Interrupt: Server shutting down.")
+        exit(-1)
+    finally:
+        close_socket(server_socket)
